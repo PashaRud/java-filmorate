5502062a032451824c18b54e4ddbf5b345cc4630
Pavel R <mjk1_126@mail.ru>
2022-06-25 10:43:34 +0300

    feat: добавить модуль и соответствующие контроллеры

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+public class User {
+
+    private int id;
+    @NotNull(message = "не указан email")
+    @NotBlank(message = "не указан email")
+    @Email(message = "некорректный формат email-а")
+    private String email;
+    @NotNull(message = "не указан login")
+    @NotBlank(message = "не указан login")
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
